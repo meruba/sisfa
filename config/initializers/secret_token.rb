752fd9122ee1be55efb1b6ee7e304f27b95ce404
Lenin Capa <lenin@shiriculapo.com>
2013-11-19 19:27:43 -0500

    proyecto modulo de farmacia

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sisfa::Application.config.secret_key_base = '141749dd0425aaa1f25e5b2dd23a9ad9366a65a88cfcd43559fdf8383c5e36331ce9849c1776fca8061e669ba4ea499dba1145a1e49c5a564d47c04ed18af79a'
