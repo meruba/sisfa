76e2f801885e80e3d7b1ee51a2b5aedbba3719de
Lenin Capa <lenin@shiriculapo.com>
2013-11-25 22:24:50 -0500

    corregido error en direccion

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -2,9 +2,9 @@ class Persona < ActiveRecord::Base
 
 #validations
 	validates :nombre, :numero_identificacion, :tipo_de_paciente, :presence => true
-	validates :nombre, :direcion, :length => { :maximum => 100 }
+	validates :nombre, :direccion, :length => { :maximum => 100 }
 	validates :numero_identificacion, :uniqueness => true
 	validates :telefono, :celular, :numericality => true
-	validates :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
+	#validates :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
 
 end
