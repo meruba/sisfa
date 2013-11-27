a9ea9ea1c06feec68baeffde88e385518544ba45
Lenin Capa <lenin@shiriculapo.com>
2013-11-27 12:01:47 -0500

    arreglado modelo persona

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,17 +1,13 @@
 class Persona < ActiveRecord::Base
 	
-	belongs_to :persona
 #validates
 	validates :nombre, :numero_identificacion, :tipo_de_paciente, :presence => true
 	validates :nombre, :direccion, :length => { :maximum => 100 }
 	validates :numero_identificacion, :uniqueness => true
 	validates :telefono, :celular, :numericality => true
-<<<<<<< HEAD
-=======
-	validates :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
+	#validates :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
 #relations
+	belongs_to :persona
 	has_many :facturas
-	
 
->>>>>>> 33c689f6c08318727b54f721335cfb91d486282b
 end
