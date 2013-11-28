e2da7e8e6699c0bf0cee4077a547011970fff92f
Lenin Capa <lenin@shiriculapo.com>
2013-11-27 21:47:57 -0500

    agregado users con sorcery (run migration)

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+
+  authenticates_with_sorcery!
+
+  #validates_confirmation_of :password
+  #validates_presence_of :password, :on => :create
+  validates_presence_of :username
+  validates_uniqueness_of :username
+
+end
+
