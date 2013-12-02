b220c1454c2d7ce50d041b34716bd306fbf8f2c5
camila <camila@camila-G31-M7-TE.(none)>
2013-12-02 11:15:29 -0500

    craedo campo rol en usuario

---
+++ db/migrate/20131202160930_agregar_campo_rol_usuario.rb
@@ -0,0 +1,10 @@
+class AgregarCampoRolUsuario < ActiveRecord::Migration
+def up
+    add_column :users, :rol, :string, :null => false
+   
+  end
+
+  def down
+  	remove_column :users, :rol
+end
+end
