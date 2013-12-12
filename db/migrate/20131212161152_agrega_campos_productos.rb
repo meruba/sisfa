0a4523e46073e9ea5cee1bab3bb8c0dacd4e3b95
camila <camila@camila-G31-M7-TE.(none)>
2013-12-12 11:27:24 -0500

    Agregado migracion de precios en producto (run migrations) y vistas de cliente

---
+++ db/migrate/20131212161152_agrega_campos_productos.rb
@@ -0,0 +1,17 @@
+class AgregaCamposProductos < ActiveRecord::Migration
+
+	def up
+		add_column :productos, :nombre_generico, :string
+		add_column :productos, :precio_b, :float
+		add_column :productos, :precio_c, :float
+		rename_column :productos, :precio, :precio_a 
+	end
+
+	def down
+		remove_column :productos, :nombre_generico
+		remove_column :productos, :precio_b
+		remove_column :productos, :precio_c
+		rename_column :productos, :precio_a, :precio
+	end
+
+end
