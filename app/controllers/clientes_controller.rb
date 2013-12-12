47eea47f1ad7220d5dc69d15c730097ab02da590
camila <camila@camila-G31-M7-TE.(none)>
2013-12-12 10:49:42 -0500

    Agregado model y num identificacion en vista new client

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -1,6 +1,6 @@
 class ClientesController < ApplicationController
   before_filter :require_login
-  before_action :set_cliente, only: [:show, :edit, :update, :destroy]
+  # before_action :set_cliente, only: [:show, :edit, :update, :destroy]
 
   # GET /clientes
   # GET /clientes.json
