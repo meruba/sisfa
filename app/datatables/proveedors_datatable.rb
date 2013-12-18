c151546541e75036672c0eaafb465274b70d8c3e
camila <camila@camila-G31-M7-TE.(none)>
2013-12-18 10:43:18 -0500

    agragado proveedorsdatatable

---
+++ app/datatables/proveedors_datatable.rb
@@ -0,0 +1,65 @@
+class ProveedorsDatatable
+  delegate :params, :link_to, :h, to: :@view
+
+  def initialize(view)
+    @view = view
+  end
+
+  def as_json(options = {})
+    {
+      sEcho: params[:sEcho].to_i,
+      iTotalRecords: Proveedor.count,
+      iTotalDisplayRecords: proveedors.total_entries,
+      aaData: data
+    }
+  end
+
+private
+
+  def data
+    proveedors.map do |proveedor|
+      [
+        (proveedor.nombre_o_razon_social),
+        (proveedor.direccion),
+        (proveedor.numero_de_identificacion),
+        (proveedor.telefono),        
+        (proveedor.codigo),
+        (proveedor.representante_legal),
+        (proveedor.pais),        
+        (proveedor.ciudad),
+        (proveedor.fax),
+         (link_to 'Editar', @view.edit_proveedor_path(proveedor), {:remote => true, 'data-toggle' =>  "modal", 'data-target' => '#myModal', class: "btn btn-warning"})
+      ]
+    end 
+  end
+
+  def proveedors
+    @proveedors ||= fetch_proveedors
+  end
+
+  def fetch_proveedors
+    proveedors = Proveedor.order("#{sort_column} #{sort_direction}")
+    proveedors = proveedors.page(page).per_page(per_page)
+    if params[:sSearch].present?
+      proveedors = proveedors.where("nombre_o_razon_social like :search or numero_de_identificacion like :search or codigo like :search", search: "%#{params[:sSearch]}%")
+    end
+    proveedors
+  end
+
+  def page
+    params[:iDisplayStart].to_i/per_page + 1
+  end
+
+  def per_page
+    params[:iDisplayLength].to_i > 0 ? params[:iDisplayLength].to_i : 10
+  end
+
+  def sort_column
+    columns = %w[nombre_o_razon_social direccion numero_de_identificacion telefono codigo represntante_legal pais ciudad fax]
+    columns[params[:iSortCol_0].to_i]
+  end
+
+  def sort_direction
+    params[:sSortDir_0] == "desc" ? "desc" : "asc"
+  end
+end
