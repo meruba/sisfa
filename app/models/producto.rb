1c9182e599f40a8117c0d517a22b7150842825f1
Lenin Capa <lenin@shiriculapo.com>
2013-12-22 12:49:11 -0500

    some validations for producto

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -3,9 +3,8 @@ class Producto < ActiveRecord::Base
 	# accepts_nested_attributes_for :proveedor
 
 #validations  
-  validates :nombre,:presence => true
-  validates :precio_a,:presence => true,
-  					:numericality => { :greater_than_or_equal_to => 0}
+  validates :nombre,:cantidad_disponible, :precio_a, :presence => true
+  validates :cantidad_disponible, :precio_a, :numericality => { :greater_than_or_equal_to => 0}
   validates :nombre, :length => { :maximum => 100 }
  
 #relations
