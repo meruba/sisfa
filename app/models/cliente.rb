0a646ff9fcb2cce8fd7bcfdb23916c28e22f5169
Lenin Capa <lenin@shiriculapo.com>
2013-12-22 12:23:17 -0500

    add validates numero_de_identificacion and nombre

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,16 +1,14 @@
 class Cliente < ActiveRecord::Base
 
+#validations
+	validates :nombre, :numero_de_identificacion,:presence => true
+	validates :nombre, :direccion, :length => { :maximum => 200 }
+	validates :numero_de_identificacion, :uniqueness => true
 # relationships
 	has_one :user
 	has_many :proformas
 	has_many :facturas
 
 #method
-	def self.search(search)
-		if search
-			where('numero_de_identificacion LIKE ?', "%#{search}%")
-		else
-			scoped
-		end
-	end
+
 end
