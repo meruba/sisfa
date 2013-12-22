eb7ae9064a1b4ed820ddab67041dc0cb741159f7
Lenin Capa <lenin@shiriculapo.com>
2013-12-22 12:22:44 -0500

    added save numero_factura in controller

--- app/controllers/facturas_controller.rb
+++ app/controllers/facturas_controller.rb
@@ -2,7 +2,7 @@ class FacturasController < ApplicationController
 	before_filter :require_login
 	
 	def new
-		@factura = Factura.new(:numero => Factura.last ? Factura.last.numero + 1 : 1 )
+		@factura = Factura.new
 		@factura.item_facturas.build
 	end
 
@@ -11,6 +11,7 @@ class FacturasController < ApplicationController
 		@cliente = cliente_attrs[:id].present? ? Cliente.update(cliente_attrs[:id],cliente_attrs) : Cliente.create(cliente_attrs)
 		if @cliente.save
 			@factura = @cliente.facturas.build(factura_params)
+			@factura.numero = Factura.last ? Factura.last.numero + 1 : 1
 			@factura.fecha_de_emision = Time.now
 			@factura.fecha_de_vencimiento = Time.now + 30.days
   	# raise 'error'
@@ -29,24 +30,24 @@ end
 
 private
 
-def factura_params
-	params.require(:factura).permit :numero,
-	:observacion,
-	:fecha_de_emision,
-	:fecha_de_vencimiento,
-	:subtotal_0,
-	:subtotal_12,
-	:descuento,
-	:iva,
-	:total,
-	:cliente_id,
-	:item_facturas_attributes => [
-		:cantidad,
-		:valor_unitario,
-		:descuento,
-		:iva,
-		:total,
-		:producto_id
-	]
-end
+	def factura_params
+		params.require(:factura).permit :numero,
+																		:observacion,
+																		:fecha_de_emision,
+																		:fecha_de_vencimiento,
+																		:subtotal_0,
+																		:subtotal_12,
+																		:descuento,
+																		:iva,
+																		:total,
+																		:cliente_id,
+																		:item_facturas_attributes => [
+																			:cantidad,
+																			:valor_unitario,
+																			:descuento,
+																			:iva,
+																			:total,
+																			:producto_id
+																		]
+	end
 end
