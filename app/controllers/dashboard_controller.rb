83a306efe446d403f439fedd7605a243f70e98ad
Fabricio Flores <fabrifloresg@gmail.com>
2013-12-27 18:50:23 -0500

    funciona pero está feísimo (pendiente css)

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,9 +3,9 @@ class DashboardController < ApplicationController
   def index
     #estas son las consultas referentes a hoy... no olvides
     @facturashoy = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
-    @facturasventanilla = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
-    @facturashospitalizacion = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
-    @facturaconsultaexterna = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
+    @facturasventanilla = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day, :tipo => "ventanilla")
+    @facturashospitalizacion = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day, :tipo => "hospitalizacion")
+    @facturaconsultaexterna = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day, :tipo => "consulta_externa")
   end
 end
 
