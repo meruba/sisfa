472fe72b9d62d937d985a5e1fcdc5897112f9650
Fabricio Flores <fabrifloresg@gmail.com>
2013-12-27 18:41:12 -0500

    added some logic to dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,11 @@
 class DashboardController < ApplicationController
   before_filter :require_login
   def index
+    #estas son las consultas referentes a hoy... no olvides
+    @facturashoy = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
+    @facturasventanilla = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
+    @facturashospitalizacion = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
+    @facturaconsultaexterna = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day)
   end
 end
+
