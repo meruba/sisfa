fe102219db18919a7c8bf1189388ba3f786e4a56
Lenin Capa <lenin@shiriculapo.com>
2013-12-29 15:55:21 -0500

    added method regla_de_tres

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,10 @@ class DashboardController < ApplicationController
     @facturasventanilla = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day, :tipo => "ventanilla")
     @facturashospitalizacion = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day, :tipo => "hospitalizacion")
     @facturaconsultaexterna = Factura.where(:created_at => Time.now.beginning_of_day..Time.now.end_of_day, :tipo => "consulta_externa")
+    @porcentaje_ventanilla = regla_de_tres(@facturasventanilla,@facturashoy)
+    @porcentaje_hospitalizacion = regla_de_tres(@facturashospitalizacion,@facturashoy)
+    @porcentaje_consultaexterna = regla_de_tres(@facturaconsultaexterna,@facturashoy)
+    # raise 'error'
     #estas son las consultas referentes al mes
     @facturasmes = Factura.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month)
     @facturasmesventanilla = Factura.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month, :tipo => "ventanilla")
@@ -16,6 +20,13 @@ class DashboardController < ApplicationController
     @facturasañoventanilla = Factura.where(:created_at => Time.now.beginning_of_year..Time.now.end_of_year, :tipo => "ventanilla")
     @facturasañohospitalizacion = Factura.where(:created_at => Time.now.beginning_of_year..Time.now.end_of_year, :tipo => "hospitalizacion")
     @facturasañoconsultaexterna = Factura.where(:created_at => Time.now.beginning_of_year..Time.now.end_of_year, :tipo => "consulta_externa")  
+  end
+
+private
+  def regla_de_tres (tipo_factura, total_facturas)
+    total = total_facturas.count
+    total_tipo_factura = tipo_factura.count
+    porcentaje = (100 * total_tipo_factura)/total
   end
 end
 
