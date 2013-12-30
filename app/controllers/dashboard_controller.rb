cc65986d178d135a9cc725285ca99751fe838770
Lenin Capa <lenin@shiriculapo.com>
2013-12-30 16:58:17 -0500

    fixed error in porcentaje

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,19 +14,27 @@ class DashboardController < ApplicationController
     @facturasmes = Factura.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month)
     @facturasmesventanilla = Factura.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month, :tipo => "ventanilla")
     @facturasmeshospitalizacion = Factura.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month, :tipo => "hospitalizacion")
-    @facturamesconsultaexterna = Factura.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month, :tipo => "consulta_externa")
+    @facturasmesconsultaexterna = Factura.where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month, :tipo => "consulta_externa")
+    @porcentajemes_ventanilla = regla_de_tres(@facturasmesventanilla,@facturasmes)
+    @porcentajemes_hospitalizacion = regla_de_tres(@facturasmeshospitalizacion,@facturasmes)
+    @porcentajemes_consultaexterna = regla_de_tres(@facturasmesconsultaexterna,@facturasmes)
+    # raise 'error'
     #estas son las consultas referentes al año
     @facturasaño = Factura.where(:created_at => Time.now.beginning_of_year..Time.now.end_of_year)
     @facturasañoventanilla = Factura.where(:created_at => Time.now.beginning_of_year..Time.now.end_of_year, :tipo => "ventanilla")
     @facturasañohospitalizacion = Factura.where(:created_at => Time.now.beginning_of_year..Time.now.end_of_year, :tipo => "hospitalizacion")
     @facturasañoconsultaexterna = Factura.where(:created_at => Time.now.beginning_of_year..Time.now.end_of_year, :tipo => "consulta_externa")  
-  end
+end
 
 private
   def regla_de_tres (tipo_factura, total_facturas)
     total = total_facturas.count
-    total_tipo_factura = tipo_factura.count
-    porcentaje = (100 * total_tipo_factura)/total
+      if total == 0
+        porcentaje = 0        
+      else
+        total_tipo_factura = tipo_factura.count
+        porcentaje = (100 * total_tipo_factura)/total
+      end
   end
 end
 
