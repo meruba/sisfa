4ad363615a7092a06b47c2aff540014f7683730f
Angel Valdez <betitos2309@gmail.com>
2014-01-08 17:57:55 -0500

    arreglado stock en proforma

--- app/models/item_proforma.rb
+++ app/models/item_proforma.rb
@@ -23,15 +23,10 @@ class ItemProforma < ActiveRecord::Base
   # validations:
   validates :cantidad, :valor_unitario, :total, :descuento, :presence => true,
                                                             :numericality => { :greater_than_or_equal_to => 0 }
-  validate :stock                                                            
+                                                   
   validate :valida_descuento
 
   # methods
-  def stock
-    if self.cantidad > producto.cantidad_disponible
-      errors.add :cantidad, "No hay suficiente stock de: " + producto.nombre
-    end
-  end
 
   def valida_descuento
     if self.descuento > self.total
