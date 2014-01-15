d7812bb0c4cd0ab4800b514bb3470bb2a8c7af9f
Fabricio Flores <fabrifloresg@gmail.com>
2014-01-15 11:35:05 -0500

    quitando warning sobre I18n

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,12 @@ module Sisfa
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'America/Lima'
+    config.time_zone = 'Quito'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
+    I18n.config.enforce_available_locales = true
   end
 end
