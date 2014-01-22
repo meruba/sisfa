7eb9d166550b87c75ab520d58336f897c95e71ae
Lenin Capa <lenin@shiriculapo.com>
2014-01-22 12:30:04 -0500

    arreglando controlador de usuarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
-  before_action :set_user, only: [:suspender]    
+  before_action :set_user, only: [:show, :suspender,:edit, :update]
   skip_before_filter :require_login, :except => [:new]
 
   def index
@@ -16,19 +16,37 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     respond_to do |format|
       format.js{ render "init" }
     end
   end
 
   def create
-    @user = User.new(current_user_params) 
+    @user = User.new(user_params)
       if @user.save
         redirect_back_or_to(users_path, notice: "Nuevo Usuario")
       else
         flash[:error] = "Ups! Algo salio mal.."
       end
+  end
+
+  def update
+    cliente_attrs = params[:user].delete :cliente_attributes
+    @cliente = Cliente.update(cliente_attrs[:id],cliente_attrs)
+    respond_to do |format|
+      if @user.update(user_params)
+        @users = User.all
+        format.html { redirect_to @user, notice: 'Usuario actualizado' }
+        format.json { render action: 'show', status: :created, location: @user }
+        format.js { render "success"}
+      else
+        # raise 'error'
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.js { render "success"}
+      end
+    end
   end
 
   def suspender
@@ -42,7 +60,7 @@ class UsersController < ApplicationController
   end
 
   private 
-    def current_user_params
+    def user_params
       params.require(:user).permit :username,
                                     :password,
                                     :password_confirmation,
@@ -50,6 +68,7 @@ class UsersController < ApplicationController
                                     :rol,
                                     :cliente_attributes => [
                                       :nombre,
+                                      :numero_de_identificacion,
                                       :direccion,
                                       :telefono,
                                       :email,
