746eedf6048635441fc6c4083cbdcfb64e51ca77
Fabricio Flores <fabrifloresg@gmail.com>
2014-01-25 19:33:46 -0500

    now working user rol

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,8 @@ authenticates_with_sorcery!
 #relationships
 	belongs_to :cliente
 	has_many :facturas
-  has_many :user_rols
-  has_many :rols, through: :user_rols
+  has_one :user_rol
+  has_one :rol, through: :user_rol
 	accepts_nested_attributes_for :cliente
 #validation
   validates_confirmation_of :password
