724593ee1feaad0e7c0d841de963bdb4c99c6f98
Fabricio Flores <fabrifloresg@gmail.com>
2014-01-29 09:33:16 -0500

    helper for validates model.errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def form_errors_for(object)
+    if object.errors.any?
+      content = pluralize object.errors.count, "error", "errores"
+      content += link_to "x", "#", :class => "close", :data => {:dismiss => "alert"}
+      content += raw "<ul>"
+      object.errors.full_messages.each do |msg|
+        content += raw content_tag :li, msg
+      end
+      content += raw "</ul>"
+      content_tag :div, id: "error_explanation", :class => "alert alert-error" do
+        raw content
+      end
+    end
+  end
 end
