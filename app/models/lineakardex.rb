ed13d4350dfa926933eb308a23858105b1bb0a97
Fabricio Flores <fabrifloresg@gmail.com>
2014-01-31 17:05:16 -0500

    fixed relationships between producto and lineakardex

--- app/models/lineakardex.rb
+++ app/models/lineakardex.rb
@@ -17,7 +17,6 @@ class Lineakardex < ActiveRecord::Base
 	validates :tipo, :fecha, :cantidad, :v_unitario, :presence => true
 	validates :cantidad, :v_unitario, :numericality => true, :numericality => { :greater_than_or_equal_to =>0 }
 #relations
-	belongs_to :producto
 	belongs_to :kardex
 
 end
