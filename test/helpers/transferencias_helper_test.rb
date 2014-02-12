2311ed3401b1378a1d7ed2557ae05e37171c9f88
Lenin Capa <lenin@shiriculapo.com>
2014-02-11 21:45:42 -0500

    rollback transferencia y item_transferencia

--- test/helpers/transferencias_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class TransferenciasHelperTest < ActionView::TestCase
-end
--- test/models/transferencia_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TransferenciaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
