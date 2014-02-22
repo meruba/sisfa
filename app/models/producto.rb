97a895954905a8996b061069ba4007ab473b5253
Fabricio Flores <fabrifloresg@gmail.com>
2014-02-22 12:11:23 -0500

    quitar los atributos que ya no existen

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -2,29 +2,23 @@
 #
 # Table name: productos
 #
-#  id                  :integer          not null, primary key
-#  nombre              :string(255)      not null
-#  precio_compra       :decimal(4, 2)    not null
-#  codigo              :string(255)
-#  categoria           :string(255)
-#  descripcion         :string(255)
-#  fecha_de_caducidad  :date
-#  casa_comercial      :string(255)
-#  nombre_generico     :string(255)
-#  precio_venta        :decimal(4, 2)
-#  cantidad_disponible :float            not null
-#  ganancia            :float            not null
+#  id              :integer          not null, primary key
+#  nombre          :string(255)      not null
+#  codigo          :string(255)
+#  categoria       :string(255)
+#  descripcion     :string(255)
+#  casa_comercial  :string(255)
+#  nombre_generico :string(255)
 #
 
 class Producto < ActiveRecord::Base
 
 #callbacks
-  before_create :set_precios
-  after_create :set_kardex
+  #before_create :set_precios
+  #after_create :set_kardex
 
 #validations  
-  validates :nombre,:cantidad_disponible, :precio_compra, :presence => true
-  validates :cantidad_disponible, :precio_compra, :numericality => { :greater_than_or_equal_to => 0}
+  validates :nombre, :presence => true
   validates :nombre, :length => { :maximum => 100 }
  
 #relations
