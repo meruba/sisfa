b9b12783b630e18f27cb24adac5e7e751f53ce62
Lenin Capa <lenin@shiriculapo.com>
2014-02-24 16:02:29 -0500

    run annotate

--- app/models/item_traspaso.rb
+++ app/models/item_traspaso.rb
@@ -2,14 +2,14 @@
 #
 # Table name: item_traspasos
 #
-#  id             :integer          not null, primary key
-#  cantidad       :float            not null
-#  valor_unitario :float            not null
-#  total          :float            not null
-#  producto_id    :integer
-#  created_at     :datetime
-#  updated_at     :datetime
-#  traspaso_id    :integer
+#  id                  :integer          not null, primary key
+#  cantidad            :float            not null
+#  valor_unitario      :float            not null
+#  total               :float            not null
+#  created_at          :datetime
+#  updated_at          :datetime
+#  traspaso_id         :integer
+#  ingreso_producto_id :integer
 #
 
 class ItemTraspaso < ActiveRecord::Base
