de2cbd11f0451f2557ef48c2dc9f68d09069d377
Lenin Capa <lenin@shiriculapo.com>
2014-02-24 17:57:50 -0500

    annotate and validations and methods

--- app/models/hospitalizacion.rb
+++ app/models/hospitalizacion.rb
@@ -1,2 +1,39 @@
+# == Schema Information
+#
+# Table name: hospitalizacions
+#
+#  id            :integer          not null, primary key
+#  fecha_emision :date             not null
+#  numero        :integer          not null
+#  subtotal      :float            not null
+#  iva           :float            not null
+#  total         :float            not null
+#  user_id       :integer
+#  cliente_id    :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
 class Hospitalizacion < ActiveRecord::Base
+	has_many :item_hospitalizacions
+	has_many :ingreso_productos, :through => :item_hospitalizacions
+  belongs_to :user
+
+	accepts_nested_attributes_for :item_hospitalizacions
+
+	#validations
+  validates :user_id, :numero, :iva, :total, :subtotal, :fecha_emision, presence: true
+  validates :subtotal, :total, :numericality => { :greater_than_or_equal_to => 0}
+  validates :numero, :numericality => { only_integer: true }
+
+	#methods
+	def self.disminuir_stock (item_hospitalizacions)
+		item_hospitalizacions.each do |item|
+			unless item.producto_id.nil?
+				producto  = Producto.find(item.producto_id)
+				producto.cantidad_disponible -= item.cantidad
+				producto.save
+			end
+		end
+	end
 end
--- app/models/item_hospitalizacion.rb
+++ app/models/item_hospitalizacion.rb
@@ -1,2 +1,48 @@
+# == Schema Information
+#
+# Table name: item_hospitalizacions
+#
+#  id                  :integer          not null, primary key
+#  cantidad            :float            not null
+#  valor_unitario      :float            not null
+#  iva                 :float            not null
+#  total               :float            not null
+#  descuento           :float
+#  created_at          :datetime
+#  updated_at          :datetime
+#  hospitalizacion_id  :integer
+#  ingreso_producto_id :integer
+#
+
 class ItemHospitalizacion < ActiveRecord::Base
+
+#relacionships
+	belongs_to :hospitalizacion
+	belongs_to :ingreso_producto
+  
+  #rollbacks
+  after_create :add_kardex_line, :disminuir_stock
+  
+  # validations
+  validates :cantidad, :valor_unitario, :total, :iva, :presence => true, :numericality => { :greater_than => 0 }
+  validate :stock
+	
+	# methods
+	def stock
+	  if self.cantidad > IngresoProducto.find(self.ingreso_producto_id).cantidad
+	  	errors.add :cantidad, "No hay suficiente stock de: " + ingreso_producto.producto.nombre
+	  end
+	end
+
+  def add_kardex_line
+    Lineakardex.create(:kardex => self.ingreso_producto.producto.kardex, :tipo => "Salida", :fecha => Time.now, :cantidad => self.cantidad, :v_unitario => self.ingreso_producto.precio_venta, :modulo => "Hospitalizacion" )
+  end
+
+  private
+  def disminuir_stock
+    disminuido = IngresoProducto.find(self.ingreso_producto_id)
+    disminuido.cantidad -= self.cantidad
+    disminuido.save
+  end
+
 end
