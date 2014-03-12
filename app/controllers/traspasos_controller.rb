3306d3eb42cac9b40e4ae293c8a72b6d82bb3e51
Lenin Capa <lenin@shiriculapo.com>
2014-03-12 15:11:24 -0500

    traspaso js

--- app/controllers/traspasos_controller.rb
+++ app/controllers/traspasos_controller.rb
@@ -10,24 +10,30 @@ class TraspasosController < ApplicationController
 	def new
 		@traspaso = Traspaso.new
 		@traspaso.item_traspasos.build
+		respond_to do |format|
+      format.html
+      format.js
+    end
 	end
 
 	def create
-		@traspaso = Traspaso.new(traspaso_params)
-		@traspaso.numero = Traspaso.last ? Traspaso.last.numero + 1 : 1
-		@traspaso.fecha_emision = Time.now
-		@traspaso.user_id = current_user.id
-		if @traspaso.save
-			redirect_to traspasos_path, :notice => "Transferencia realizada"
-		else
-			flash[:error] = 'Error al realizar Transferencia'
-		end
+		respond_to do |format|
+			@traspaso = Traspaso.new(traspaso_params)
+			@traspaso.numero = Traspaso.last ? Traspaso.last.numero + 1 : 1
+			@traspaso.fecha_emision = Time.now
+			@traspaso.user_id = current_user.id
+			@traspaso.save
+  		format.js
+		end	
 	end
 
 	def show
 		@traspaso = Traspaso.find(params[:id])
 		respond_to do |format|
-      format.js{ render "show" }
+      format.js
+      format.pdf do
+				render :pdf => "proforma", :layout => 'report.html', :template => "traspasos/traspaso_pdf.html.erb"
+			end
     end
 	end
 
@@ -44,6 +50,7 @@ class TraspasosController < ApplicationController
 		:item_traspasos_attributes => [
 			:cantidad,
 			:valor_unitario,
+			:iva,
 			:total,
 			:ingreso_producto_id
 		]
