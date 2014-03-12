34bfb1c9abc5a99f2461e9d0ae9ba9397d178299
Lenin Capa <lenin@shiriculapo.com>
2014-03-11 22:29:52 -0500

    delete old create proformas

--- app/controllers/proformas_controller.rb
+++ app/controllers/proformas_controller.rb
@@ -41,28 +41,6 @@ class ProformasController < ApplicationController
   	end
   end
   
-	def create_old
-		cliente_attrs = params[:proforma].delete :cliente
-		@cliente = cliente_attrs[:id].present? ? Cliente.update(cliente_attrs[:id],cliente_attrs) : Cliente.create(cliente_attrs)
-		if @cliente.save
-			@proforma = @cliente.proformas.build(proforma_params)
-			@proforma.numero = Proforma.last ? Proforma.last.numero + 1 : 1
-			@proforma.fecha_emision = Time.now
-			if @proforma.save
-      	render :pdf => "proforma", :layout => 'report.html', :template => "proformas/proforma_pdf.html.erb"
-				# redirect_to proformas_path, :notice => "Proforma Guardada"
-			else
-				render action: 'new' 
-				flash[:error] = 'Error en la Proforma'
-			end
-		else
-			flash[:error] = 'Errores en el cliente' 
-			@proforma = Proforma.new(:numero => Proforma.last ? Proforma.last.numero + 1 : 1 )
-			@proforma.item_proformas.build
-			render 'new'
-		end
-	end
-
 	def facturar
 		@proforma = Proforma.find(params[:id])
 		@factura = @proforma.cliente.facturas.build(:tipo => "venta", :tipo_venta=>"ventanilla", :subtotal_0 => @proforma.subtotal_0, :subtotal_12 => @proforma.subtotal_12, :descuento => @proforma.descuento, :iva => @proforma.iva, :total => @proforma.total)
