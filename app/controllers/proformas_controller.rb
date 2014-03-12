dc3052d29b9f40e62fd042732865febb23da0291
Lenin Capa <lenin@shiriculapo.com>
2014-03-11 22:27:08 -0500

    now proforma print from modal when save!

--- app/controllers/proformas_controller.rb
+++ app/controllers/proformas_controller.rb
@@ -12,15 +12,36 @@ class ProformasController < ApplicationController
 	def new
 		@proforma = Proforma.new
 		@proforma.item_proformas.build
+		respond_to do |format|
+      format.html
+      format.js
+    end
 	end
 
 	def show
-    respond_to do |format|
-      format.js
-    end
+		respond_to do |format|
+			format.js
+			format.pdf do
+				render :pdf => "proforma", :layout => 'report.html', :template => "proformas/proforma_pdf.html.erb"
+			end
+		end
+	end
+
+  def create
+  respond_to do |format|
+  	cliente_attrs = params[:proforma].delete :cliente
+  	@cliente = cliente_attrs[:id].present? ? Cliente.update(cliente_attrs[:id],cliente_attrs) : Cliente.create(cliente_attrs)
+  		if @cliente.save
+  			@proforma = @cliente.proformas.build(proforma_params)
+  			@proforma.numero = Proforma.last ? Proforma.last.numero + 1 : 1
+  			@proforma.fecha_emision = Time.now
+  			@proforma.save
+      end
+  		format.js
+  	end
   end
   
-	def create
+	def create_old
 		cliente_attrs = params[:proforma].delete :cliente
 		@cliente = cliente_attrs[:id].present? ? Cliente.update(cliente_attrs[:id],cliente_attrs) : Cliente.create(cliente_attrs)
 		if @cliente.save
