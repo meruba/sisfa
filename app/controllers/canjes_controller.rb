041c7cf13c5f265689e5cebfd827b41c95ec388a
Lenin Capa <lenin@shiriculapo.com>
2014-03-19 21:25:20 -0500

    now save lote and fecha

--- app/controllers/canjes_controller.rb
+++ app/controllers/canjes_controller.rb
@@ -6,21 +6,27 @@ class CanjesController < ApplicationController
   end
 
   def save_nuevo
-    @canje = Canje.new
-    mismo_producto = params[:canje].delete :entrada_nueva
-    lote = mismo_producto[:lote]
-    fecha = mismo_producto[:fecha_caducida]
-  	@canje.antiguo_id = @actual.id
-  	@canje.tipo = "Mismo producto"
-  	if @canje.save
-  		redirect_to productos_path
-      flash[:notice] = 'Canje Realizado'
-  	else
-  		raise
-  	end
+    canje = Canje.new
+    if params[:tipo] = "mismo_producto"
+      mismo_producto = params[:canje].delete :entrada_nueva
+      lote = mismo_producto[:lote]
+      fecha = mismo_producto[:fecha_caducidad]
+      canje.antiguo_id = @actual.id
+      canje.mismo_producto(fecha,lote)
+      if canje.save
+        redirect_to productos_path
+        flash[:notice] = 'Canje Realizado'
+      else
+        redirect_to productos_path
+        flash[:error] = 'Error en canje'
+      end
+    else
+      #falta si es nuevo
+    end
   end
 
   private
+  
   def set_ingreso
   	@actual = IngresoProducto.find(params[:id])
   end
--- app/models/canje.rb
+++ app/models/canje.rb
@@ -19,33 +19,25 @@ class Canje < ActiveRecord::Base
     belongs_to :producto
 
 #validations
-	# validates :fecha, :tipo, :presence =>true
+	validates :fecha, :tipo, :presence =>true
 	
 #callbacks
-  after_create :set_values
-  after_save :set_salida_kardex, :update_producto
+  # after_create :set_values
+  # after_save :set_salida_kardex, :update_producto
 
 #methods
+  def mismo_producto(fecha,lote)
+    self.fecha = Time.now
+    self.tipo = "Mismo Producto"
+    ingreso  = find_ingreso()
+    Lineakardex.create(:kardex => ingreso.producto.kardex, :tipo => "Salida", :fecha => Time.now, :cantidad => ingreso.cantidad, :v_unitario => ingreso.producto.precio_compra, :observaciones => "Producto Caducado")
+    IngresoProducto.create(:producto_id => ingreso.producto.id, :cantidad => ingreso.cantidad, :fecha_caducidad => fecha, :lote => lote)
+    ingreso.update(:cantidad => '0')
+  end
+
   private
 
   def find_ingreso
 		ingreso = IngresoProducto.find(self.antiguo_id)
-  end
-
-	def set_values
-  	ingreso = find_ingreso()
-	  self.fecha = Time.now
-  	self.producto_id = ingreso.producto.id
-	end
-
-  def set_salida_kardex
-  	ingreso = find_ingreso()
-    Lineakardex.create(:kardex => ingreso.producto.kardex, :tipo => "Salida", :fecha => Time.now, :cantidad => ingreso.cantidad, :v_unitario => ingreso.producto.precio_compra, :observaciones => "Producto Caducado")
-  end
-
-  def update_producto
-  	ingreso = find_ingreso()
-    IngresoProducto.create(:producto_id => ingreso.producto.id, :cantidad => ingreso.cantidad, :fecha_caducidad => "2015/06/23", :lote => "234")
-    ingreso.update(:cantidad => '0')
   end
 end
