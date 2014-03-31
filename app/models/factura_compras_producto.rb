fab2d67a0bb0d095470ab8573833feba434b91bf
leninkp <leviskp87@gmail.com>
2014-03-31 11:10:45 -0500

    now calculated values for items factura_compra

--- app/models/factura_compras_producto.rb
+++ app/models/factura_compras_producto.rb
@@ -5,6 +5,10 @@
 #  id                :integer          not null, primary key
 #  factura_compra_id :integer
 #  producto_id       :integer
+#  cantidad          :float            not null
+#  valor_unitario    :float            not null
+#  iva               :float            not null
+#  total             :float            not null
 #
 
 class FacturaComprasProducto < ActiveRecord::Base
@@ -20,10 +24,36 @@ class FacturaComprasProducto < ActiveRecord::Base
     accepts_nested_attributes_for :producto
   
   #methods
-    def producto_attributes=(attributes)
-      if attributes['id'].present?
-        self.producto = Producto.find(attributes['id'])
+  def producto_attributes=(attributes)
+    cantidad = 0
+    if attributes['id'].present?
+      self.producto = Producto.find(attributes['id'])
+      attributes[:ingreso_productos_attributes].each do |value,key|
+        cantidad = cantidad + key[:cantidad].to_f
       end
-      super
+      if attributes['hasiva'] == "0"
+        iva = 0
+      else
+        iva = (attributes['precio_compra'].to_f * 0.12).round(2)
+      end
+      self.cantidad = cantidad
+      self.valor_unitario = producto.precio_compra
+      self.iva  = iva
+      self.total = self.valor_unitario * self.cantidad
+    else
+      if attributes['hasiva'] == "0"
+        iva = 0
+      else
+        iva = (attributes['precio_compra'].to_f * 0.12).round(2)
+      end
+      attributes[:ingreso_productos_attributes].each do |value,key|
+        cantidad = cantidad + key[:cantidad].to_f
+      end
+      self.cantidad = cantidad
+      self.valor_unitario = attributes['precio_compra']
+      self.iva = iva
+      self.total = self.cantidad * self.valor_unitario
     end
+    super
+  end
 end
