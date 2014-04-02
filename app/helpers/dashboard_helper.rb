315e765587b561afaa1a949367c37915920a8e09
leninkp <leviskp87@gmail.com>
2014-04-02 10:46:45 -0500

    changed params query canje

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -81,7 +81,7 @@ module DashboardHelper
 
   def liquidacion(tiempo)
     query_reports(tiempo)
-    @canjes = Canje.where(:created_at => Time.now.beginning_of_month...Time.now.end_of_month)
+    @canjes = Canje.where(:created_at => tiempo)
     @canjes_cantidad = @canjes.count()
   end
 end
