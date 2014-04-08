b8e59593d260c35acec34cb8352021067008148d
leninkp <leviskp87@gmail.com>
2014-04-08 11:33:59 -0500

    visual changes

--- app/datatables/productos_datatable.rb
+++ app/datatables/productos_datatable.rb
@@ -22,7 +22,7 @@ private
         (producto.nombre),
         #(producto.nombre_generico),
         (producto.codigo),
-        (producto.categoria),
+        (producto.casa_comercial),
         (link_to '', @view.edit_producto_path(producto), {:remote => true, :rel => 'tooltip', :title => 'Editar', 'data-toggle' =>  "modal", 'data-target' => '#myModal', class: "ttip fa fa-pencil btn btn-warning btn-xs only-admin"}) + " " + (link_to '', producto, :remote => true, :rel => 'tooltip', :title=> 'Mostrar','data-toggle' =>  "modal", 'data-target' => '#myModal', class: "ttip fa fa-eye btn btn-info mostrar btn-xs") + " " + (link_to '', @view.producto_kardex_index_path(producto), {:rel => 'tooltip', :title => 'Kardex', class: "ttip fa fa-stack-exchange btn btn-success btn-xs"}) ,
         #(producto.casa_comercial)
       ]
@@ -37,7 +37,7 @@ private
     productos = Producto.order("#{sort_column} #{sort_direction}")
     productos = productos.page(page).per_page(per_page)
     if params[:sSearch].present?
-      productos = productos.where("nombre like :search or codigo like :search or categoria like :search", search: "%#{params[:sSearch]}%")
+      productos = productos.where("nombre like :search or codigo like :search or casa_comercial like :search", search: "%#{params[:sSearch]}%")
     end
     productos
   end
