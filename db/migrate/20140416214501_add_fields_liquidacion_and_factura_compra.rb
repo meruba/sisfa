c7468de117840a8f9ca2cc65981a3a12870e9a72
leninkp <leviskp87@gmail.com>
2014-04-16 17:43:46 -0500

    added some fields

---
+++ db/migrate/20140416214501_add_fields_liquidacion_and_factura_compra.rb
@@ -0,0 +1,14 @@
+class AddFieldsLiquidacionAndFacturaCompra < ActiveRecord::Migration
+  def up
+    add_column :liquidacions, :subtotal12_compra, :float, :default => 0
+    add_column :liquidacions, :anulados_ventanilla, :integer, :default => 0
+    add_column :factura_compras, :subtotal_12, :float
+  end
+
+  def down
+  	remove_column :liquidacions, :subtotal12_compra
+    remove_column :liquidacions, :anulados_ventanilla
+    remove_column :factura_compras, :subtotal_12
+  end
+end
+  
