1b4908d0aa91d4ede6f7d197501acfc26ec22646
leninkp <leviskp87@gmail.com>
2014-04-21 14:23:30 -0500

    added controller paciente

---
+++ app/controllers/pacientes_controller.rb
@@ -0,0 +1,2 @@
+class PacientesController < ApplicationController
+end
---
+++ test/controllers/pacientes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PacientesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
