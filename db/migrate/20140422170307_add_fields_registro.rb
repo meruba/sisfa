2dd44ee17163d25e1bf0909d34a31a8f1b27a0b0
leninkp <leviskp87@gmail.com>
2014-04-28 20:55:01 -0500

    removed historia_clinica unnecesary, now is include in paciente

--- db/migrate/20140422170307_add_fields_registro.rb
+++ db/migrate/20140422170307_add_fields_registro.rb
@@ -1,6 +1,5 @@
 class AddFieldsRegistro < ActiveRecord::Migration
   def change
-  	add_reference :registros, :historia_clinica, index: true
   	add_column :registros, :diagnostico_ingreso, :string
   	add_column :registros, :diagnostico_salida, :string
   	add_column :registros, :discapacidad, :string
--- test/models/historia_clinica_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HistoriaClinicaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
