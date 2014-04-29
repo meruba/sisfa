391e28a0d7923677f973cadaafeb5fdd99e866b5
leninkp <leviskp87@gmail.com>
2014-04-28 21:07:38 -0500

    added views and methods to paciente and registros

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,15 +25,19 @@ module ApplicationHelper
   end
 
   def local_date(date, tipo)
-    case tipo
-    when tipo = "date"
-      date = date.strftime("%Y-%m-%d")
-    when tipo = "datetime"
-      date = date.strftime("%Y-%m-%d | %H:%M:%S")
-    when tipo = "time"
-      date = date.strftime("%H:%M:%S")
+    if date.nil? == true
+      date = "sin fecha"
+    else
+      case tipo
+      when tipo = "date"
+        date = date.strftime("%Y-%m-%d")
+      when tipo = "datetime"
+        date = date.strftime("%Y-%m-%d | %H:%M:%S")
+      when tipo = "time"
+        date = date.strftime("%H:%M:%S")
+      end
     end
-      date 
+    date 
   end
 
 end
