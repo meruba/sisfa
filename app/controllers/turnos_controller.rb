bbc71a73e0d0b2605f6ceadc77804621287803db
leninkp <leviskp87@gmail.com>
2014-04-30 22:15:07 -0500

    making turnos

--- app/controllers/turnos_controller.rb
+++ app/controllers/turnos_controller.rb
@@ -1,18 +1,21 @@
 class TurnosController < ApplicationController
 	def index
-		
+		@turnos = Turno.all
 	end
 
 	def new
 		@turno = Turno.new
+		respond_to do |format|
+      format.html
+			format.js
+		end
 	end
 
 	def create
 		@turno = Turno.new(turno_params)
-		if @turno.save
-			redirect_to pacientes_path, :notice => "Almacenado"
-		else
-			render action: 'new'
+		respond_to do |format|
+			@registro.save
+			format.js { render "success" }
 		end
 	end
 
