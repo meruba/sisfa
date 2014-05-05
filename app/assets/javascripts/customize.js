3d25830886fb3e572cd87bd28173324b21162ec0
leninkp <lenin@shiriculapo.com>
2014-05-04 21:25:12 -0500

    visual changes

--- app/assets/javascripts/customize.js
+++ app/assets/javascripts/customize.js
@@ -1,31 +1,15 @@
 $(document).ready(function() {
-    
-    var navListItems = $('ul.setup-panel li a'),
-        allWells = $('.setup-content');
-
-    allWells.hide();
-
-    navListItems.click(function(e)
-    {
-        e.preventDefault();
-        var $target = $($(this).attr('href')),
-            $item = $(this).closest('li');
-        
-        if (!$item.hasClass('disabled')) {
-            navListItems.closest('li').removeClass('active');
-            $item.addClass('active');
-            allWells.hide();
-            $target.show();
-        }
-    });
-    
-    $('ul.setup-panel li.active a').trigger('click');
-    
-    // DEMO ONLY //
-    $('#activate-step-2').on('click', function(e) {
-        $('ul.setup-panel li:eq(1)').removeClass('disabled');
-        $('ul.setup-panel li a[href="#step-2"]').trigger('click');
-        $(this).remove();
-    })    
+  // change genero icon
+  genero = $(".genero").text()
+  if (genero = "GENERO:FEMENINO") {
+    $(".genero-icon").removeClass("fa-male").addClass("fa-female");
+  };
+  // result = $('#atendido li').length >= 1
+  // console.log(result);
+  // if (result == false ) {
+  //   $("#atendido").html("<span class='text-muted'>No hay Turnos<span>")
+  // };
 });
+
+
 
