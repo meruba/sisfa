b36d3c4db791beb9093becf1c3697616e6fc27cb
leninkp <lenin@shiriculapo.com>
2014-05-17 20:43:58 -0500

    new migrations

--- db/migrate/20140515193852_create_condicions.rb
+++ db/migrate/20140515193852_create_condicions.rb
@@ -4,13 +4,14 @@ class CreateCondicions < ActiveRecord::Migration
     	t.references :paciente, index: true
     	t.references :doctor, index: true
     	t.string :medico_asignado
+        t.string :motivo_de_consulta
     	t.string :antecedentes_personales
     	t.string :antecedentes_familiares
     	t.string :enfermedad_actual
     	t.string :revision_organos_sistema
     	t.string :presion_arterial
     	t.string :pulso
-    	t.string :temperatua
+    	t.string :temperatura
     	t.string :examen_fisico
     	t.string :diagnostico_1
     	t.string :diagnostico_2
