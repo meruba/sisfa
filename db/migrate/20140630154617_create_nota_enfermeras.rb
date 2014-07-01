1a994beb75995535147a6ac18f694d3e740ec126
leninkp <leviskp87@gmail.com>
2014-07-01 15:12:35 -0500

    added some relations in database

--- db/migrate/20140630154617_create_nota_enfermeras.rb
+++ db/migrate/20140630154617_create_nota_enfermeras.rb
@@ -1,7 +1,7 @@
 class CreateNotaEnfermeras < ActiveRecord::Migration
   def change
     create_table :nota_enfermeras do |t|
-    	t.references :hospitalizacion, index: true
+    	t.references :hospitalizacion_registro, index: true
       t.timestamps
     end
   end
