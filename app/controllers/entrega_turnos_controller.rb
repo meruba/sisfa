cd474c2a5e35f37cf10491af8803dbacb9bbd741
leninkp <leviskp87@gmail.com>
2014-07-16 18:49:39 -0500

    beter sql

--- app/controllers/entrega_turnos_controller.rb
+++ app/controllers/entrega_turnos_controller.rb
@@ -1,6 +1,6 @@
 class EntregaTurnosController < ApplicationController
-  before_action :find_entrega, only: [:show]
-  before_action :find_entrega_to_item, only: [:view_create_item]
+	before_action :find_entrega, only: [:show]
+	before_action :find_entrega_to_item, only: [:view_create_item]
 	
 	def index
 		@hoja = EntregaTurno.new
@@ -11,26 +11,29 @@ class EntregaTurnosController < ApplicationController
 	def new
 		@hoja = EntregaTurno.new
 		respond_to do |format|
-      format.js
-    end
+			format.js
+		end
 	end
 
 	def create
 		@hoja = EntregaTurno.new(entrega_turno_params.merge(fecha: Time.now))
 		@hoja.save
 		respond_to do |format|
-      format.js
-    end
+			format.js
+		end
 	end
 
 	def show
-		items = @entrega.item_entrega_turnos.includes(hospitalizacion_registro: [:paciente => :cliente])
-		@examenes = items.where(:tipo_item => "examen")
-		@ayunas = items.where(:tipo_item => "ayuna")
-		@aislamientos = items.where(:tipo_item => "aislamiento")
-		@defunciones = items.where(:tipo_item => "defuncion")
-		@movimientos = items.where(:tipo_item => "movimiento")
-		@graves = items.where(:tipo_item => "grave")
+		@items = @entrega.item_entrega_turnos.includes([hospitalizacion_registro: [:paciente => :cliente]],[:user])
+		@examenes , @ayunas, @aislamientos, @defunciones, @movimientos, @graves = [], [], [], [], [], []
+		@items.each do |item|
+			@examenes << item if item.tipo_item == 'examen'
+			@ayunas << item if item.tipo_item == 'ayuna'
+			@aislamientos << item if item.tipo_item == 'aislamiento'
+			@defunciones << item if item.tipo_item == 'defuncione'
+			@movimientos << item if item.tipo_item == 'movimiento'
+			@graves << item if item.tipo_item == 'grave'
+		end
 	end
 
 	def view_create_item
@@ -42,14 +45,14 @@ class EntregaTurnosController < ApplicationController
 
 	def entrega_turno_params
 		params.require(:entrega_turno).permit(:fecha,
-      :servicio)
+			:servicio)
 	end
 
 	def find_entrega
-  	@entrega = EntregaTurno.find(params[:id])		
+		@entrega = EntregaTurno.find(params[:id])		
 	end
 
 	def find_entrega_to_item
-  	@entrega = EntregaTurno.find(params[:entrega_turno_id])	
+		@entrega = EntregaTurno.find(params[:entrega_turno_id])	
 	end
 end
