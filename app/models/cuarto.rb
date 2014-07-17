374de7dafdec2cc1ae841419b6e6fca76e09c41d
leninkp <leviskp87@gmail.com>
2014-07-17 13:33:27 -0500

    added some validations

--- app/models/cuarto.rb
+++ app/models/cuarto.rb
@@ -9,7 +9,16 @@
 #
 
 class Cuarto < ActiveRecord::Base
-	has_many :camas, dependent: :destroy
+	has_many :camas
 	validates :nombre, :presence =>true
 	validates_uniqueness_of :nombre, :case_sensitive => false #nombre es unico sea escrito mayuscula o minuscula
+	before_destroy :check_cuartos
+
+	private
+	def check_cuartos
+		unless camas.empty?     
+			self.errors[:base] << "No se puede eliminar tiene camas ingresadas."
+			return false   
+		end 
+	end
 end
