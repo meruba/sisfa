9847b095528658a794dfbccfd5a426ad21bd95dc
leninkp <leviskp87@gmail.com>
2014-08-03 15:18:04 -0500

    finish views users

--- app/controllers/panel_aplication_controller.rb
+++ app/controllers/panel_aplication_controller.rb
@@ -1,13 +1,14 @@
 class PanelAplicationController < ApplicationController
 	before_filter :require_login
-	before_action :only_super_user
+	before_action :only_admins
+	
 	def index		
 	end
 
 	private
-	#get access all views as super user
-	def only_super_user
-		if current_user.rol == Rol.administrador
+	#get access views as admin
+	def only_admins
+		if current_user.rol == Rol.administrador or current_user.rol == Rol.administrador_farmacia or current_user.rol == Rol.administrador_estadistica
 			@admin =  true
 		end
 	end
