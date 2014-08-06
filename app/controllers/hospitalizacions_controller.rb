0c7550f3554654813537cb6491a7083d2b74d5d6
leninkp <leviskp87@gmail.com>
2014-08-06 18:18:27 -0500

    hospitalizacion

--- app/controllers/hospitalizacions_controller.rb
+++ app/controllers/hospitalizacions_controller.rb
@@ -12,7 +12,7 @@ class HospitalizacionsController < ApplicationController
   end
 
   def pedidos
-		@pedidos =  Hospitalizacion.includes(:hospitalizacion_registro).where("hospitalizacion_registros.alta_enfermeria = false").references(:hospitalizacion_registro)
+		@pedidos =  Hospitalizacion.includes(hospitalizacion_registro: [paciente: [:cliente]]).where("hospitalizacion_registros.alta_enfermeria = false").references(hospitalizacion_registro: [paciente: [:cliente]])
   end
 
 	def show_pedido
@@ -60,14 +60,15 @@ class HospitalizacionsController < ApplicationController
 	end
 
 	def find_hospitalizacion
-		@hospitalizacion = Hospitalizacion.find(params[:id])
+		@hospitalizacion = Hospitalizacion.includes(item_hospitalizacions: [ingreso_producto: [:producto]]).where(id: params[:id]).references(item_hospitalizacions: [ingreso_producto: [:producto]]).first
 	end
 
-	def find_user
-		if current_user.rol == Rol.administrador_farmacia or current_user.rol == Rol.vendedor
-			@permiso =  false
-		else
-			@permiso = true
+	def find_user #views
+		if current_user.rol == Rol.administrador_farmacia or current_user.rol == Rol.vendedor or current_user.rol == Rol.administrador
+			@permiso_farmacia = true
+		end
+		if current_user.rol == Rol.administrador_enfermeria or current_user.rol == Rol.enfermera or current_user.rol == Rol.administrador
+			@permiso_enfermeria = true
 		end
 	end
 end
--- app/controllers/item_hospitalizacions_controller.rb
+++ app/controllers/item_hospitalizacions_controller.rb
@@ -4,7 +4,6 @@ class ItemHospitalizacionsController < ApplicationController
 	before_filter :is_admin_or_vendedor_farmacia, :only => [:despachar]
 	before_action :find_user, :only => [:create]
   before_action :find_item, :only => [:despachar]
-  # before_filter :is_admin_or_enfermera_enfermeria
   
   def create
 		@item = ItemHospitalizacion.new(item_hospitalizacion_params.merge(pedido_por: current_user.cliente.nombre))
@@ -46,11 +45,12 @@ class ItemHospitalizacionsController < ApplicationController
 		@item = ItemHospitalizacion.find(params[:id])
 	end
 
-	def find_user
-		if current_user.rol == Rol.administrador_farmacia or current_user.rol == Rol.vendedor
-			@permiso =  false
-		else
-			@permiso = true
+	def find_user #views
+		if current_user.rol == Rol.administrador_farmacia or current_user.rol == Rol.vendedor or current_user.rol == Rol.administrador
+			@permiso_farmacia = true
+		end
+		if current_user.rol == Rol.administrador_enfermeria or current_user.rol == Rol.enfermera or current_user.rol == Rol.administrador
+			@permiso_enfermeria = true
 		end
 	end
 end
