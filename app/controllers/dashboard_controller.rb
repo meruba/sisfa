2ce9297ada1c40b0bdde81dab4506e2dbfb77cd8
Fabricio Flores <fabrifloresg@gmail.com>
2014-08-08 16:59:13 -0500

    remove some trash

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,20 +2,6 @@ class DashboardController < ApplicationController
   before_filter :require_login
   before_filter :is_admin_or_vendedor_farmacia
   
-  include DashboardHelper
-  
-  def index
-    ventas(Time.now.beginning_of_day..Time.zone.now)
-  end
-
-  def estadisticas_dia
-    ventas(Time.now.beginning_of_day..Time.zone.now)
-  end
-
-  def estadisticas_mes
-    ventas(Time.now.beginning_of_month..Time.now.end_of_month)
-  end
-
   def generar_reporte
     @start_date = params[:fecha_inicial]
     @end_date = params[:fecha_final]
@@ -32,26 +18,6 @@ class DashboardController < ApplicationController
     end
   end
 
-  def reportes_cierre_caja_diario
-    @fecha = params[:fecha].to_time
-    caja_dia(@fecha.beginning_of_day..@fecha.end_of_day)
-    if @ventanilla_cantidad == 0
-      render :template => "results/not_result"
-    else
-      render :pdf => "reporte", :layout => 'report.html', :template => "dashboard/reportes/pdf_caja_dia.html.erb"
-    end
-  end
-
-  def reportes_cierre_caja_mensual
-    @fecha = params[:fecha]
-    @liquidacion = Liquidacion.where(:fecha => @fecha).first
-    if @liquidacion.nil?
-      render :template => "results/not_result"
-    else
-      render :pdf => "reporte", :layout => 'report.html', :template => "dashboard/reportes/pdf_caja_mes.html.erb"
-    end
-  end
-
   def liquidaciones
     @fecha = params[:fecha]
     liquidacion(@fecha)
@@ -64,30 +30,6 @@ class DashboardController < ApplicationController
       end
     else
       render :template => "results/not_result"
-    end
-  end
-
-  def cierre_de_caja_dia
-    caja_dia
-    respond_to do |format|
-      format.html
-      format.js
-      format.pdf do
-        cerrar_caja_dia
-        render :pdf => "reporte", :layout => 'report.html', :template => "dashboard/reportes/pdf_caja_dia.html.erb"
-      end
-    end
-  end
-
-  def cierre_de_caja_mes
-    @fecha = Time.now.beginning_of_month.to_date
-    caja_mes(@fecha)
-    respond_to do |format|
-      format.html
-      format.js
-      format.pdf do
-        render :pdf => "reporte", :layout => 'report.html', :template => "dashboard/reportes/pdf_caja_mes.html.erb"
-      end
     end
   end
 end
--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -1,4 +1,6 @@
 class ReportesController < ApplicationController
+  before_filter :require_login
+  
   def index
     
   end
