bdb749f4edb99f5198ddd64ced4980b62eabe900
Fabricio Flores <fabrifloresg@gmail.com>
2014-08-08 08:38:07 -0500

    fixing migration... need to destroy db and migrate all

--- db/migrate/20140807155640_change_decimal_liquidacion.rb
+++ db/migrate/20140807155640_change_decimal_liquidacion.rb
@@ -1,34 +1,34 @@
 class ChangeDecimalLiquidacion < ActiveRecord::Migration
 	def up
-		change_column :liquidacions, :iva_ventanilla, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :iva_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :iva_traspaso, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :iva_compra, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal_ventanilla, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal_traspaso, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal_compra, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_ventanilla, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_traspaso, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_compra, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal12_ventanilla, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal12_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal12_traspaso, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal12_compra, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal12_venta, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :subtotal_venta, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :iva_venta, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_venta, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_sin_iva_ventanilla, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_sin_iva_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_sin_iva_traspaso, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_sin_iva_compra, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :total_sin_iva_venta, :decimal, precision: 5, scale: 2, default: 0
-		change_column :liquidacions, :saldo_anterior, :decimal, precision: 7, scale: 2, default: 0
-		change_column :liquidacions, :saldo_final, :decimal, precision: 7, scale: 2, default: 0
-		change_column :liquidacions, :costo_venta, :decimal, precision: 7, scale: 2, default: 0
-		change_column :liquidacions, :utilidad, :decimal, precision: 7, scale: 2, default: 0
+		change_column :liquidacions, :iva_ventanilla, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :iva_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :iva_traspaso, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :iva_compra, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal_ventanilla, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal_traspaso, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal_compra, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_ventanilla, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_traspaso, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_compra, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal12_ventanilla, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal12_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal12_traspaso, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal12_compra, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal12_venta, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :subtotal_venta, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :iva_venta, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_venta, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_sin_iva_ventanilla, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_sin_iva_hospitalizacion, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_sin_iva_traspaso, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_sin_iva_compra, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :total_sin_iva_venta, :decimal, precision: 5, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :saldo_anterior, :decimal, precision: 7, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :saldo_final, :decimal, precision: 7, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :costo_venta, :decimal, precision: 7, scale: 2, default: 0.00, null: false
+		change_column :liquidacions, :utilidad, :decimal, precision: 7, scale: 2, default: 0.00, null: false
 	end
 	def down
 		change_column :liquidacions, :iva_ventanilla, :float, default: 0
