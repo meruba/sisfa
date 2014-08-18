d13aa22d9e54f8bcc0d8721db914f58f892c5c2a
leninkp <leviskp87@gmail.com>
2014-08-18 09:58:35 -0500

    fixed not show pdf

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -8,14 +8,14 @@ class ReportesController < ApplicationController
   def reporte_total
     @start_date = params[:fecha_inicial]
     @end_date = params[:fecha_final]
-    @tipo_factura = params[:tipo_factura] 
+    @tipo_factura = params[:tipo] 
     respond_to do |format|
       format.html{
         query_reports(params[:fecha_inicial].to_time.beginning_of_day..params[:fecha_final].to_time.end_of_day, @tipo_factura)
       }
       format.pdf do
         query_reports(params[:fecha_inicial].to_time.beginning_of_day..params[:fecha_final].to_time.end_of_day, @tipo_factura)
-        render :pdf => "reporte", :layout => 'report.html', :template => "generar_reporte.html.erb", :orientation => 'Landscape'
+        render :pdf => "reporte", :layout => 'report.html', :template => "reportes/reporte_total.html.erb", :orientation => 'Landscape'
       end
       format.js
     end
