41c4d46f27dfe7a1c964750513dc0712ef751080
leninkp <leviskp87@gmail.com>
2014-10-03 12:03:55 -0500

    redesign proformas to receta

--- app/controllers/proformas_controller.rb
+++ app/controllers/proformas_controller.rb
@@ -41,11 +41,11 @@ class ProformasController < ApplicationController
 		@factura.fecha_de_emision = Time.now
 		@factura.fecha_de_vencimiento = Time.now + 30.days
 		@factura.item_facturas = Factura.create_items_facturas(@proforma.item_proformas)
-		if @factura.save
-			redirect_to facturas_path, :notice => "Factura Creada"
-		else
-			redirect_to proformas_path
-			flash[:error] = 'Error al crear la factura'
+		respond_to do |format|
+			@factura.save
+			@proforma.facturado = true
+			@proforma.save
+			format.js
 		end
 	end
 	
--- app/datatables/proformas_datatable.rb
+++ app/datatables/proformas_datatable.rb
@@ -22,9 +22,7 @@ private
         (proforma.cliente.nombre),
         (proforma.numero),
         (proforma.total),
-        (link_to '', proforma, :remote => true, :rel=> 'tooltip', :title=>'Mostrar','data-toggle' =>  "modal", 'data-target' => '#myModal', class: "ttip fa fa-eye btn btn-info") + " " + (link_to '', @view.facturar_proforma_path(proforma), method: :post, data: { confirm: 'Esta seguro?' }, :rel => "tooltip", :title => 'Facturar', class: "ttip fa fa-thumbs-up btn btn-success")
-        # (link_to 'Mostrar', @view.show(factura), {:remote => true, 'data-toggle' =>  "modal", 'data-target' => '#myModal', class: "btn btn-warning"})
-        # (link_to '', @view.show_factura_path(factura), {:remote => true, 'data-toggle' =>  "modal", 'data-target' => '#myModal', class: "btn btn-warning"})
+        (link_to '', proforma, :remote => true, :rel=> 'tooltip', :title=>'Mostrar','data-toggle' =>  "modal", 'data-target' => '#myModal', class: "ttip fa fa-eye btn btn-info")
       ]
     end
   end
@@ -34,7 +32,7 @@ private
   end
 
   def fetch_proformas
-    proformas = Proforma.order("#{sort_column} #{sort_direction}")
+    proformas = Proforma.where(:facturado => false).order("#{sort_column} #{sort_direction}")
     proformas = proformas.page(page).per_page(per_page)
     if params[:sSearch].present?
       proformas = proformas.where("numero like :search", search: "%#{params[:sSearch]}%")
