f832ece1f8c19a08dc7dea4c74bd3299d956b5b9
leninkp <leviskp87@gmail.com>
2014-10-14 18:10:45 -0500

    added print list turnos: today and tomorrow

--- app/controllers/turnos_controller.rb
+++ app/controllers/turnos_controller.rb
@@ -1,6 +1,6 @@
 class TurnosController < ApplicationController
 	before_filter :require_login
-  before_filter :is_admin_or_auxiliar_estadistica
+	before_filter :is_admin_or_auxiliar_estadistica
 	before_action :set_turno, only: [:atendido, :siguiente_dia, :edit, :update, :destroy]
 
 	def index
@@ -8,23 +8,31 @@ class TurnosController < ApplicationController
 	end
 
 	def hoy
-		@turnos = Doctor.list_turnos_query(Time.now.beginning_of_day..Time.now.end_of_day)
-		if @turnos.empty?
-			render "results/not_turnos"			
+		respond_to do |format|
+			@turnos = Doctor.list_turnos_query(Time.now.beginning_of_day..Time.now.end_of_day)
+			@fecha =  Time.now
+			format.html
+			format.pdf do
+				render :pdf => "turnos de hoy", :layout => 'report.html', :template => "turnos/print_turnos.html.erb"
+			end
 		end
 	end
 
 	def manana
 		@turnos = Doctor.list_turnos_query(Time.now.tomorrow.beginning_of_day..Time.now.tomorrow.end_of_day)
-		if @turnos.empty?
-			render "results/not_turnos"			
+		@fecha =  Time.now.tomorrow
+		respond_to do |format|
+			format.html
+			format.pdf do
+				render :pdf => "turnos de manana", :layout => 'report.html', :template => "turnos/print_turnos.html.erb"
+			end
 		end
 	end
 
 	def new
 		@turno = Turno.new
 		respond_to do |format|
-      format.js
+			format.js
 		end
 	end
 
@@ -32,7 +40,7 @@ class TurnosController < ApplicationController
 		@turno = Turno.new(turno_params)
 		if @_params[:hoy] == "1" #obtiene valor del check_box_tag
 			@turno.fecha = Time.now.beginning_of_day
-			else			
+		else			
 			@turno.fecha = Time.now.tomorrow.beginning_of_day #fecha para el proximo dia
 		end
 		respond_to do |format|
@@ -46,19 +54,19 @@ class TurnosController < ApplicationController
 
 	def edit
 		respond_to do |format|
-      format.js
-    end
+			format.js
+		end
 	end
 
-  def update
-    respond_to do |format|
-      @turno.update(turno_params)
-      format.js {
+	def update
+		respond_to do |format|
+			@turno.update(turno_params)
+			format.js {
 				@turnos = Doctor.turnos_doctores
-      	render "change"
-      }
-    end
-  end
+				render "change"
+			}
+		end
+	end
 
 	def destroy
 		@turno.destroy
