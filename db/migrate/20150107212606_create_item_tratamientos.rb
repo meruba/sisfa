d4e448ecd82fca718e0871f0217d1fcdaca34e43
leochuron <leo.churon@gmail.com>
2015-01-07 16:41:47 -0500

    cree campos a tablas tarataiento e itemtratamiento

--- db/migrate/20150107212606_create_item_tratamientos.rb
+++ db/migrate/20150107212606_create_item_tratamientos.rb
@@ -1,6 +1,9 @@
 class CreateItemTratamientos < ActiveRecord::Migration
   def change
     create_table :item_tratamientos do |t|
+    	t.string :codigo
+    	t.string :nombre
+    	t.references :tratamiento, index: true
 
       t.timestamps
     end
