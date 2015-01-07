bb3768bcd2071699636c328fa7ae80d7291bf01c
leochuron <leo.churon@gmail.com>
2015-01-07 17:20:31 -0500

    algunas mofificaciones

--- db/migrate/20150107213802_create_personals.rb
+++ db/migrate/20150107213802_create_personals.rb
@@ -2,7 +2,6 @@ class CreatePersonals < ActiveRecord::Migration
   def change
     create_table :personals do |t|
     	t.references :cliente, index: true
-
       t.timestamps
     end
   end
--- db/migrate/20150107220203_create_resultado_tratamientos.rb
+++ db/migrate/20150107220203_create_resultado_tratamientos.rb
@@ -1,7 +1,8 @@
 class CreateResultadoTratamientos < ActiveRecord::Migration
   def change
     create_table :resultado_tratamientos do |t|
-
+    	t.references :personal, index: true
+    	t.string :resultado
       t.timestamps
     end
   end
