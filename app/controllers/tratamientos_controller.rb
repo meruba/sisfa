f6c633199117a68be88be9dabb1aae72fd177b8f
leochuron <leo.churon@gmail.com>
2015-01-08 12:14:33 -0500

    configurar metodos en controlador

--- app/controllers/tratamientos_controller.rb
+++ app/controllers/tratamientos_controller.rb
@@ -1,2 +1,28 @@
 class TratamientosController < ApplicationController
+	def new 
+	@tratamiento = Tratamiento.new
+	end
+	def create
+	@tratamiento = Tratamiento.new(tratamiento_params)
+	if @tratamineto.save
+		redirect_to new_tratamiento_path, :notice => "GUARDADO"
+	else
+		redirect_to new_tratamiento_path, :notice => "NO GUARDADO"	
+		end	
+	end
+	def edit
+				
+	end
+	def update
+				
+	end
+	def anular
+				
+	end	
+	
+	private
+
+	def tratamiento_params
+		params.require(:tratamiento).permit(:nombre, :numeracion)	
+	end	
 end
