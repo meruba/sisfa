07ae60dad4cf17bfaaf1949b2cdc1235d9ed6ace
leochuron <leo.churon@gmail.com>
2015-01-08 12:42:23 -0500

    vistas para taratamiento

--- app/controllers/tratamientos_controller.rb
+++ app/controllers/tratamientos_controller.rb
@@ -1,23 +1,26 @@
 class TratamientosController < ApplicationController
+	def index
+		
+	end
 	def new 
-	@tratamiento = Tratamiento.new
+		@tratamiento = Tratamiento.new #CREA UN OBJETO TIPO TRATAMIENTO 
 	end
 	def create
-	@tratamiento = Tratamiento.new(tratamiento_params)
-	if @tratamineto.save
-		redirect_to new_tratamiento_path, :notice => "GUARDADO"
-	else
-		redirect_to new_tratamiento_path, :notice => "NO GUARDADO"	
+		@tratamiento = Tratamiento.new(tratamiento_params)
+		if @tratamiento.save
+			redirect_to new_tratamiento_path, :notice => "GUARDADO"
+		else
+			redirect_to new_tratamiento_path, :notice => "NO GUARDADO"	
 		end	
 	end
 	def edit
-				
+
 	end
 	def update
-				
+
 	end
 	def anular
-				
+
 	end	
 	
 	private
