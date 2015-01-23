df63eb9420c5d3cdb09437efd8395a5255f87bc5
leochuron <leo.churon@gmail.com>
2015-01-22 22:51:24 -0500

    comenzando a hacer calendario horarios disponibles

--- app/models/resultado_tratamiento.rb
+++ app/models/resultado_tratamiento.rb
@@ -15,5 +15,38 @@
 
 class ResultadoTratamiento < ActiveRecord::Base
 	belongs_to :asignar_horario
+	has_one :disponibilidad_horario
 	validates :resultado, :personal_id, :presence => true, :on => :update
+	validate :isfull
+	after_save :add_disponiblidad
+	accepts_nested_attributes_for :disponibilidad_horario
+
+	def isfull
+		numero_de_turnos = DisponiblidadHorario.where(:dia => self.fecha.beginning_of_day..self.fecha.end_of_day).count()
+		if numero_de_turnos == 4
+			errors.add :asignar_horario_id, "Turnos llenos para:" + self.asignar_horario.horario.hora
+		end
+	end
+
+	def add_disponiblidad
+		unless DisponiblidadHorario.last.nil?
+			numero_de_turnos = DisponiblidadHorario.where(:dia => self.fecha.beginning_of_day..self.fecha.end_of_day).count()
+			if numero_de_turnos == 1
+			raise
+				horario = DisponiblidadHorario.where(:dia => self.fecha.beginning_of_day..self.fecha.end_of_day).last
+				horario.lleno = true
+				horario.save
+			else
+				horario = DisponiblidadHorario.new
+				horario.dia = self.fecha
+				horario.resultado_tratamiento = self
+				horario.save
+			end
+		else
+			horario = DisponiblidadHorario.new
+			horario.dia = self.fecha
+			horario.resultado_tratamiento = self
+			horario.save
+		end
+	end
 end
