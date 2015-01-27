e49bc8346157c06e136cb05cc59e14645ccd598e
leochuron <leo.churon@gmail.com>
2015-01-27 09:44:45 -0500

    trabajando en el calendario

--- app/controllers/calendario_controller.rb
+++ app/controllers/calendario_controller.rb
@@ -5,7 +5,8 @@ class CalendarioController < ApplicationController
   	@numero_dias = numero_dias(Time.now)
   	@espacios = number_space(Time.now)
   	@fecha = Time.now
-  	@turnos = ResultadoTratamiento.where(:fecha => Time.now.beginning_of_month..Time.now.end_of_month)
+    # @turnos = ResultadoTratamiento.where(:fecha => Time.now.beginning_of_month..Time.now.end_of_month)
+  	@turnos = DisponiblidadHorario.where(:dia => Time.now.beginning_of_month..Time.now.end_of_month)
   end
 
   def next_month
--- app/controllers/resultado_tratamientos_controller.rb
+++ app/controllers/resultado_tratamientos_controller.rb
@@ -1,2 +1,6 @@
 class ResultadoTratamientosController < ApplicationController
+	def by_day
+		fecha = params[:dia].to_date
+		@turnos = ResultadoTratamiento.where(:fecha => fecha.beginning_of_day..fecha.end_of_day)
+	end
 end
