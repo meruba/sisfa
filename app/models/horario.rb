026878ab7fa190d171d7a511a15e980d35a0135a
Lenin Capa <leviskp87@gmail.com>
2015-02-21 11:56:54 -0500

    Changes in horario

--- app/models/horario.rb
+++ app/models/horario.rb
@@ -2,15 +2,23 @@
 #
 # Table name: horarios
 #
-#  id         :integer          not null, primary key
-#  hora       :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id          :integer          not null, primary key
+#  hora        :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#  hora_inicio :string(255)
+#  hora_final  :string(255)
 #
 
 class Horario < ActiveRecord::Base
 	# belongs_to :paciente
 	belongs_to :asignar_horario
-	validates :hora, :presence =>true
+	validates :hora_inicio, :hora_final, :presence =>true
 	validates_uniqueness_of :hora, :case_sensitive => false #nombre es unico sea escrito mayuscula o minuscula
+
+  before_save :set_values
+
+  def set_values
+    self.hora = self.hora_inicio + " - " + self.hora_final
+  end
 end
