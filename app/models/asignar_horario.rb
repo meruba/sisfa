8fdf4b5b7d8a4efd3a02ad085e08fe79cd96dc03
Lenin Capa <leviskp87@gmail.com>
2015-02-22 13:55:21 -0500

    new fields and changes in asignar horario

--- app/models/asignar_horario.rb
+++ app/models/asignar_horario.rb
@@ -9,14 +9,19 @@
 #  created_at      :datetime
 #  updated_at      :datetime
 #  paciente_id     :integer
+#  numero_factura  :integer
+#  total_factura   :float            default(0.0)
+#  diagnostico     :string(255)
 #
 
 class AsignarHorario < ActiveRecord::Base
 	belongs_to :paciente
 	has_many :resultado_tratamientos, dependent: :destroy
 	has_many :tratamiento_registros, dependent: :destroy
+
+	validates :paciente_id, :numero_factura, :total_factura, :diagnostico, :presence => true
 	# validates :numero_terapias, :fecha_inicio, :item_tratamiento_id, :presence => true
-	validates :numero_terapias, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 40, :message => "Rango maximo de 0-40 terapias" }
+	# validates :numero_terapias, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 40, :message => "Rango maximo de 0-40 terapias" }
 
 	accepts_nested_attributes_for :resultado_tratamientos, :tratamiento_registros
 
