179cc4fc09ace5043a27330b22f8cc01c1a9f266
Lenin Capa <leviskp87@gmail.com>
2015-04-06 15:33:55 -0500

    add edit registro turno fisiatria

--- app/controllers/asignar_horarios_controller.rb
+++ app/controllers/asignar_horarios_controller.rb
@@ -1,6 +1,6 @@
 class AsignarHorariosController < ApplicationController
 include DashboardHospitalHelper
-
+  before_action :find_horario, only: [:edit, :update, :destroy]
   def autocomplete
     respond_to do |format|
       format.json { render :json => AsignarHorario.autocomplete(params[:term]) }
@@ -23,7 +23,24 @@ include DashboardHospitalHelper
 	end
 
   def view_edit
-    @registros = AsignarHorario.last(10)
+    @registros = AsignarHorario.includes(paciente: [:cliente], tratamiento_registros: [:item_tratamiento]).last(10)
+    # turnos = Turno.includes(paciente: [:cliente]).where(:fecha => Time.now.beginning_of_day..Time.now.end_of_day).references(paciente: [:cliente])
+    # @paciente = Paciente.includes(:cliente, :informacion_adicional_paciente, :condicions).where(:id => params[:paciente_id]).references(:cliente, :informacion_adicional_paciente, :condicions).first
+
+  end
+
+  def edit
+    @tratamientos =  ItemTratamiento.where(:suspendido => false)
+    respond_to do |format|
+      format.js{ render "new" }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      @horario.update(horario_params)
+      format.js
+    end
   end
 
 	def create
@@ -47,18 +64,26 @@ include DashboardHospitalHelper
 			:total_factura,
 			:doctor_remitente,
 			:diagnostico,
+      :id,
 			:tratamiento_registros_attributes => [
 				:nombre_tratamiento,
-				:asignar_horario_id,
 				:item_tratamiento_id,
+        :asignar_horario_id,
+        :id,
 				:_destroy
 			],
 			:resultado_tratamientos_attributes => [
 				:fecha,
 				:horario_id,
 				:paciente_id,
+        :asignar_horario_id,
+        :id,
 				:_destroy
 			]
 			)
 	end
+
+  def find_horario
+    @horario =  AsignarHorario.find(params[:id])
+  end
 end
